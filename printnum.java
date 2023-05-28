ae0026c4ee2a9afa0987467ac410e401997ec857
Aman Kumar Sah <118824856+shahaman098@users.noreply.github.com>
2023-05-28 22:25:13 +0545

    It is a simple code of printing a number.

---
+++ printnum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class printnum
+{
+	public static void main(String[] args)
+	{
+		//print the result
+		System.out.println("Output is : ");
+
+		//loop  to print 1 to 10.
+		for(int i = 1; i <= 10; i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
