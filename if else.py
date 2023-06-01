ab36694c5ce456f356a62bbf47789d5c1de39408
Aman Kumar Sah <118824856+shahaman098@users.noreply.github.com>
2023-06-01 23:36:19 +0545

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,10 @@
+def function()
+x= int(input("Enter a number:"))
+if (x>0):
+    print("x is greater than zero")
+elif (x==0):
+    print("x is zero number")
+else:
+    print("x is less than 0")
+
+print('number checking program in over.')
